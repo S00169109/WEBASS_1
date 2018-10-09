0dade03a00f3c2fab98588fd758d6526602351a0
Shane Kilgannon <S00169109@mail.itsligo.ie>
2018-10-09 13:03:16 +0100

    Progress on Filtering

--- src/app/bike-list/bike-list.component.ts
+++ src/app/bike-list/bike-list.component.ts
@@ -7,18 +7,30 @@ import { IBikeData } from '../ibike-data';
 })
 export class BikeListComponent implements OnChanges {
 @Input() dispData:IBikeData[];
+FilterBikes:IBikeData[];
 check:boolean;
   constructor() {
     this.check = true;
    }
-   FilterItems():void{
-     
+   FilterItems(value:string):void{
+
+     console.log(value)
+    this.FilterBikes = value ? this.Filter(value):this.dispData;
+    
+      console.log(this.FilterBikes)
+   }
+   Filter(item:string):IBikeData[]{
+      console.log(this.FilterBikes)
+      return this.FilterBikes.filter((bike:IBikeData) => bike.category.indexOf(item.toLocaleLowerCase()) != -1)
    }
     ToggleImg():boolean{
      return this.check = !this.check
     }
   ngOnChanges() {
-
+    if (this.dispData) {
+      this.FilterBikes = this.dispData
+    }
+      
   }
 
 }
