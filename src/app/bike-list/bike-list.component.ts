a394e577c2335bd9954b0473195dcc4a4bd2025a
Shane Kilgannon <S00169109@mail.itsligo.ie>
2018-10-09 10:55:53 +0100

    Toggle Img working

--- src/app/bike-list/bike-list.component.ts
+++ src/app/bike-list/bike-list.component.ts
@@ -7,9 +7,16 @@ import { IBikeData } from '../ibike-data';
 })
 export class BikeListComponent implements OnChanges {
 @Input() dispData:IBikeData[];
+check:boolean;
   constructor() {
+    this.check = true;
    }
-
+   FilterItems():void{
+     
+   }
+    ToggleImg():boolean{
+     return this.check = !this.check
+    }
   ngOnChanges() {
 
   }
